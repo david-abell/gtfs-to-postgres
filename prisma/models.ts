bada1bb2501bf48a43a61f6979dd416828793845
David Abell <79927957+david-abell@users.noreply.github.com>
2023-12-08 11:28:10 +0000

    rename models file

--- prisma/models.ts
+++ prisma/models.ts
@@ -10,6 +10,20 @@ import {
 } from "@prisma/client";
 import { CamelKeysToSnake } from "../types/utils";
 
+export type CalendarDateWithoutId = Omit<CalendarDate, "id">;
+export type ShapeWithoutId = Omit<Shape, "id">;
+export type StopTimeWithoutId = Omit<StopTime, "id">;
+
+export type ModelWithoutId =
+  | Agency
+  | Calendar
+  | Omit<CalendarDate, "id">
+  | Route
+  | Trip
+  | Stop
+  | Omit<Shape, "id">
+  | Omit<StopTime, "id">;
+
 type AgencySnakeCase = CamelKeysToSnake<Agency>;
 type CalendarSnakeCase = CamelKeysToSnake<Calendar>;
 type CalendarDateSnakeCase = CamelKeysToSnake<CalendarDate>;
