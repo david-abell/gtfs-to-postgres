56d5bb990eaaa21495c164844d0eccc1aefbcbdd
David Abell <79927957+david-abell@users.noreply.github.com>
2023-12-12 16:54:36 +0000

    first working version

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,13 @@
 import "dotenv/config";
 import { consola } from "consola";
 import { temporaryDirectory } from "tempy";
-import { rename } from "fs/promises";
-import { exec } from "child_process";
 import { downloadFiles } from "./download";
-import { importFile } from "./importFile";
 import { parseFile } from "./parseFile";
 
-import { existsSync, mkdirSync, readFileSync } from "fs";
-import prisma from "./client";
+import { readFileSync } from "fs";
 import pgClientPool from "./pgClientPool";
 import { PoolClient } from "pg";
+import { resolveFilePath } from "./utils";
 
 const FILE_IMPORT_ORDER = [
   "agency.txt",
@@ -24,42 +21,65 @@ const FILE_IMPORT_ORDER = [
 ];
 
 async function main() {
-  const pgClient = await pgClientPool.connect();
-  const downloadDir = "./tmp";
-  // const downloadDir = temporaryDirectory();
-  // await downloadFiles(downloadDir);
-  // await archiveDB();
-  await prepareFreshDB(pgClient);
+  const downloadDir = temporaryDirectory();
+  await downloadFiles(downloadDir);
 
   const t0 = performance.now();
+  const pgClient = await pgClientPool.connect();
 
-  console.profile();
   try {
+    await prepareFreshDB(pgClient);
+
+    console.profile();
     for (const file of FILE_IMPORT_ORDER) {
-      // await importFile(`${downloadDir}/${file}`);
       await parseFile(`${downloadDir}/${file}`, pgClient);
     }
+
+    const t1 = performance.now();
+    consola.info(`Importing files took ${(t1 - t0) / 1000} seconds.`);
+
+    await finalizeTables(pgClient);
+    console.profileEnd();
   } catch (error) {
-    consola.error(error);
+    if (error instanceof Error) {
+      console.log(error.message);
+    } else {
+      console.log("There was an unknown error.");
+    }
+    process.exit(1);
   } finally {
     pgClient.release();
   }
 
-  const t1 = performance.now();
-  consola.info(`Importing files took ${(t1 - t0) / 1000} seconds.`);
-  console.profileEnd();
   await pgClientPool.end();
+  process.exit(0);
 }
 
 main();
 
 async function prepareFreshDB(pgClient: PoolClient) {
-  const sql = readFileSync("./sql/seedTables.sql", "utf8");
+  const sql = readFileSync(resolveFilePath("./sql/seedTables.sql"), "utf8");
   try {
     await pgClient.query(sql);
   } catch (error) {
     if (error instanceof Error) {
-      consola.error(error.message);
+      consola.error(`Error seeding database: ${error.message}`);
+    }
+  }
+}
+
+async function finalizeTables(pgClient: PoolClient) {
+  const relations = readFileSync(
+    resolveFilePath("./sql/setRelations.sql"),
+    "utf8"
+  );
+  const indexes = readFileSync(resolveFilePath("./sql/setIndexes.sql"), "utf8");
+  try {
+    await pgClient.query(relations);
+    await pgClient.query(indexes);
+  } catch (error) {
+    if (error instanceof Error) {
+      consola.error(`Error seeding database: ${error.message}`);
     }
   }
 }
