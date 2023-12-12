e33b20ede5f52da1c2f324eb7a6bfdbaf4ad0351
David Abell <79927957+david-abell@users.noreply.github.com>
2023-12-12 16:50:11 +0000

    fix: ingestStream not expecting CSV, support all files

--- src/parseFile.ts
+++ src/parseFile.ts
@@ -1,25 +1,18 @@
-import { createReadStream, existsSync, createWriteStream } from "fs";
+import { createReadStream, existsSync } from "fs";
 import consola from "consola";
 import { parse } from "csv-parse";
 import { transform } from "stream-transform";
 import { stringify } from "csv-stringify";
-import { fileURLToPath } from "url";
-import { dirname, join } from "path";
 import { pipeline } from "stream/promises";
 import { from } from "pg-copy-streams";
 
 import { basename, extname } from "path";
-import { formatLine } from "./utils";
+import { formatLine, getTableNameAndColumns } from "./utils";
 import { PoolClient } from "pg";
 
 export async function parseFile(sourcePath: string, dbClient: PoolClient) {
-  const currentFilePath = fileURLToPath(import.meta.url);
-  const __dirname = dirname(currentFilePath);
   const extension = extname(sourcePath);
   const fileName = basename(sourcePath, extension);
-  const filePath = `../tmp_parsed/${fileName}.csv`;
-
-  const destinationPath = join(__dirname, filePath);
 
   if (!existsSync(sourcePath)) {
     return;
@@ -31,8 +24,7 @@ export async function parseFile(sourcePath: string, dbClient: PoolClient) {
     );
   }
 
-  // testing agency file
-  if (fileName !== "agency") return;
+  const tableNameAndColumns = getTableNameAndColumns(fileName);
 
   consola.start(`Importing ${fileName}${extension}`);
 
@@ -48,35 +40,11 @@ export async function parseFile(sourcePath: string, dbClient: PoolClient) {
     return formatLine(record);
   });
 
-  const stringifier = stringify({
-    bom: true,
-    header: true,
-  });
+  const readStream = createReadStream(sourcePath, "utf-8");
+  const ingestStream = dbClient.query(
+    from(`COPY ${tableNameAndColumns} FROM STDIN WITH (FORMAT CSV)`)
+  );
 
-  const resetTableQuery = `BEGIN TRANSACTION;
-    DROP TABLE IF EXISTS agency;
-    
-    CREATE TABLE agency (
-      agency_id       TEXT NOT NULL
-                           PRIMARY KEY,
-      agency_name     TEXT NOT NULL,
-      agency_url      TEXT NOT NULL,
-      agency_timezone TEXT NOT NULL
-    );
-
-    COMMIT TRANSACTION;
-  `;
-
-  try {
-    await dbClient.query(resetTableQuery);
-    const readStream = createReadStream(sourcePath, "utf-8");
-    // const writeStream = createWriteStream(destinationPath);
-    const ingestStream = dbClient.query(from(`COPY agency FROM STDIN`));
-
-    await pipeline(readStream, parser, transformer, stringifier, ingestStream);
-  } catch (error) {
-    consola.error(error);
-  }
-
-  consola.success(`Finished writing records.`);
+  await pipeline(readStream, parser, transformer, stringify(), ingestStream);
+  consola.success(`Finished writing records to table ${fileName}.`);
 }
