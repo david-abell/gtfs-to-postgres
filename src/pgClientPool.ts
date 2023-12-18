4095fecc99780796216c68e4d297e75f9ec58c28
David Abell <79927957+david-abell@users.noreply.github.com>
2023-12-18 17:34:04 +0000

    fix: pgClientPool connect with connection string

--- src/pgClientPool.ts
+++ src/pgClientPool.ts
@@ -1,11 +1,7 @@
 import pg from "pg";
 
 const config = {
-  user: "postgres",
-  host: "localhost",
-  database: "gtfs",
-  password: process.env.PG_PASSWORD,
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
 };
 
 export const pgClientPool = new pg.Pool(config);
